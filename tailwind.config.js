4601805f2928847cc99c3401015d1f6092e457ba
rakamahesa29 <rakamahesa29@gmail.com>
2024-05-06 08:45:02 +0700

    add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.liquid",
+    "./**/*.liquid"
+  ],
+  theme: {
+    colors: {
+      'GreenSolid': '#005750',
+      'WhiteMilk': '#FFFAF6',
+      'GreenToDark': '#D9D9D9',
+      'GreenToGone': '#005750'
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
